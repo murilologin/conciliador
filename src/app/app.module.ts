b1e62c218d91e5e8c80ecafcd4aaddb12eabab07
Jorge Murilo <murilo.login@gmail.com>
2023-09-19 09:42:22 -0300

    ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PoFieldModule, PoModule, PoUploadComponent } from '@po-ui/ng-components';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { PoTemplatesModule } from '@po-ui/ng-templates';
+import { NgxCsvParserModule } from 'ngx-csv-parser';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PoModule,
+    HttpClientModule,
+    RouterModule.forRoot([]),
+    PoTemplatesModule,
+    PoFieldModule,
+    NgxCsvParserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
